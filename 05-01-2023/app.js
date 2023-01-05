9bb7f326d25803d2d0af7397f80731424a880804
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-05 17:09:37 +0500

    05-01-2023

---
+++ 05-01-2023/app.js
@@ -0,0 +1,136 @@
+// alert(2=="2")
+//Strict equals/not equals
+// alert(10!="10")
+// var email=prompt("Enter your email")
+// var password=prompt("Enter Password")
+// alert(email==="someone@example.com"||password==="12345")
+// alert("Sameer"=="Sameer")
+
+// var num=1
+// num+=3
+
+// alert("The new value for num is"+num)
+
+// var age=+prompt("Enter your age")
+
+// if(age===18){
+//     alert("You are eligible to vote")
+// }
+// else{
+//     alert("You are not eligible to vote")
+// }
+
+// var number=+prompt("Enter a number")
+// if(parseInt(number)%2===0){
+//     alert("The number is even")
+// }
+
+// else if(parseInt(number)%2!==0){
+//     alert("The number is odd")
+// }
+// else{
+//    alert("Your input is not a number")
+// }
+
+// var item=prompt("Kiya Laye ho?")
+// if(item==="tomato"){
+//     alert("bohat zabardast")
+// }
+// else if(item==="yogurt"){
+// alert("Chlo kuch tou mila")
+// }
+// else{
+//     alert("Tum ksi kaam k nahi ho")
+// }
+
+
+// var age=+prompt("Enter age")
+// var salary=+prompt("Enter Salary")
+
+// if(age>=25&&salary>30000){
+//     alert("Done")
+// }
+// else if(age<25&&salary>30000){
+//     alert("Your age is not 25")
+// }
+// else if(age>=25&&salary<30000){
+//     alert("Your Salary is too less")
+// }
+// else{
+//     alert("Not done")
+// }
+
+// var month=+prompt("Enter month number")
+// if(month===1){
+//     alert("January has 31 days")
+// }
+// else if(month===2){
+//     alert("February has 28 or 29 days")
+// }
+// else if(month===3){
+//     alert("March has 31 days")
+// }
+
+// else if(month===4){
+//     alert("April has 30 days")
+// }
+
+// else if(month===5){
+//     alert("May has 31 days")
+// }
+// else{
+//     alert("Invalid month")
+// }
+
+// var email=prompt("Enter your email")
+// var password=prompt("Enter Password")
+// if(email==="someone@example.com"&&password==="12345"){
+//     alert("Logged in")
+// }
+// else if(email==="someone@example.com"&&password!=="12345"){
+//     alert("Entered password is incorrect")
+// }
+
+// else if(email!=="someone@example.com"&&password==="12345"){
+//     alert("Entered email is incorrect")
+// }
+// else{
+//     alert("Invalid password and email")
+// }
+
+// var userName=prompt("Enter your email")
+// var password=prompt("Enter Password")
+
+// if(userName==="sameer"){
+//     if(password==="12345"){
+//         alert("Logged in")
+//     }
+//     else{
+// alert("Email is correct but password is wrong")
+//     }
+// }
+// else{
+//     alert("your email or password is wrong")
+// }
+
+// var number1=+prompt("Enter first number")
+// var number2=+prompt("Enter second number")
+
+// var sum=number1+number2
+
+// if(sum>10)
+// {
+//     number1=number1*2
+//     alert("The new value of number1: "+number1)
+// }
+// else if(sum<10){
+//     number2=number2*2
+//     alert("The new value of number2: "+number2)
+// }
+
+// var num1=+prompt("enter first number")
+// var num2=+prompt("enter second number")
+
+// var result=num1-(-num2)
+
+// alert(result)
